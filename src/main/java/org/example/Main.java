f1614d0a7c4d2b6370c619f89df9aebdb5f8d48b
Mayank-Bansal <mayank06570@gmail.com>
2025-07-10 17:36:00 +0530

    ONE TO MANY AND VICE VERSA.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,30 +5,57 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        Laptop laptop = new Laptop();
-        laptop.setBrand("ASUS");
-        laptop.setModel("Rog");
-        laptop.setRam(16);
+        Laptop l1 = new Laptop();
+        l1.setLid(1);
+        l1.setBrand("Hp");
+        l1.setModel("15s");
+        l1.setRam(16);
+
+        Laptop l2 = new Laptop();
+        l2.setLid(2);
+        l2.setBrand("Dell");
+        l2.setModel("Book");
+        l2.setRam(32);
+
+        Laptop l3 = new Laptop();
+        l3.setLid(3);
+        l3.setBrand("Macbook");
+        l3.setModel("m3");
+        l3.setRam(16);
 
         Alien alien = new Alien();
-        alien.setAid(102);
-        alien.setAname("Chandan");
+        alien.setAid(101);
+        alien.setAname("Mayank");
         alien.setTech("Java");
-        alien.setLaptop(laptop);
+        alien.setLaptop(Arrays.asList(l1,l2));
+
+        Alien alien1 = new Alien();
+        alien1.setAid(101);
+        alien1.setAname("Mayank");
+        alien1.setTech("Java");
+        alien1.setLaptop(Arrays.asList(l1,l2));
+
+        l1.setAlien(alien);
+        l2.setAlien(alien);
 
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Alien.class)
+                .addAnnotatedClass(Laptop.class)
                 .buildSessionFactory();
 
         Session session = sessionFactory.getCurrentSession();
         Transaction transaction = session.beginTransaction();
-        alien = session.byId(Alien.class).load(102);
-        System.out.println(alien);
+
+        session.persist(l1);
+        session.persist(l2);
         session.persist(alien);
+
         transaction.commit();
         session.close();
         sessionFactory.close();
